fa4a44e4291a06dfbe75915d343432cb0b56ca74
Farid Ali <faridalicode@gmail.com>
2020-12-15 20:29:02 +0530

    replace example game with a new file structure

--- example/example_game.rb
+++ example/example_game.rb
@@ -1,5 +1,6 @@
-require_relative "../lib/tic_tac_toe.rb"
- 
+root = File.expand_path("../", File.dirname(__FILE__))
+require "#{root}/lib/tic_tac_toe.rb"
+
 puts "Welcome to tic tac toe"
 bob = TicTacToe::Player.new({symbol: "X", name: "bob"})
 frank = TicTacToe::Player.new({color: "O", name: "frank"})
